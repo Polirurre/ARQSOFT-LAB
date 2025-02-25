b809249c650b2752b94acc3373e81985c13bdd69
Sara <sara.martinez.mardones@estudiantat.upc.edu>
2025-02-25 16:47:42 +0100

    mynumber

--- LAB1/MyNumber.py
+++ LAB1/MyNumber.py
@@ -1,14 +1,12 @@
 class MyNumber():
-    value: float
 
-    function __init__(self, value):
+    def __init__(self, value: float):
         self.value = float(value)
     
-    function getValue(self):
+    def getValue(self) -> float:
         return self.value
 
-
 mi_numero = MyNumber(15)
-print(f"atributo value {mi_numero.getValue()}")
-posible_valor_estatico = MyNumber.value
-print(posible_valor_estatico)
+print(f"atributo value: {mi_numero.getValue()}")
+#posible_valor_estatico = MyNumber.value
+#print(posible_valor_estatico)
