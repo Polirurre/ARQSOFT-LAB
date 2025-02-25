fb706f219163481495edfadfdbe6a5cb9e59ee7e
Pol Irurre <irurrepol@gmail.com>
2025-02-25 16:39:04 +0100

    Co-authored-by: Sara <saramartzzz@users.noreply.github.com>

--- LAB1/MyNumber.py
+++ LAB1/MyNumber.py
@@ -1,1 +1,14 @@
+class MyNumber():
+    value: float
 
+    function __init__(self, value):
+        self.value = float(value)
+    
+    function getValue(self):
+        return self.value
+
+
+mi_numero = MyNumber(15)
+print(f"atributo value {mi_numero.getValue()}")
+posible_valor_estatico = MyNumber.value
+print(posible_valor_estatico)
